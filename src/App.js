839eaf553f62a1a3395e2f48811fb9d01fdfdab6
Paula Cassia Feltrin <93276824+paulafeltrin@users.noreply.github.com>
2022-07-13 08:27:13 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+// Crie um Component de function e retorne uma frase na tela
+
+import React from "react";
+import "./styles.css";
+
+function App() {
+  return <h1>Estou usando Component de function</h1>;
+}
+
+export default App;
